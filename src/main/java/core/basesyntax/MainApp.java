e520067ed4d5e977bcd647246837b2ac327a84dc
Artem <artem.kozakevych@gmail.com>
2026-07-24 20:13:33 +0300

    Fix abstarct class)

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,7 +2,7 @@ package core.basesyntax;
 
 public class MainApp {
 
-    static void main() {
+    public static void main(String[] args) {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
