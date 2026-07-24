88cbb22a9d1b68faf9c25c27ec8cd166bed462fe
Artem <artem.kozakevych@gmail.com>
2026-07-24 20:10:55 +0300

    Fix abstarct class!

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -6,7 +6,6 @@ public class MainApp {
         Machine excavator = new Excavator();
         Machine bulldozer = new Bulldozer();
         Machine truck = new Truck();
-
         Machine[] machines = new Machine[] {excavator, bulldozer, truck};
 
         for (Machine machine : machines) {
